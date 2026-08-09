3048d8dd5134941a1734e55ff856e111959b3fd3
Haohao Zhang <zhhos98@gmail.com>
2026-08-10 04:05:13 +0800

    Chain Dublin failure source pass after Harvard receiving skill

--- sources/sources-pass17.js
+++ sources/sources-pass17.js
@@ -50,4 +50,9 @@
   });
 
   consularSection.insertAdjacentElement('afterend', section);
+
+  const next = document.createElement('script');
+  next.src = 'sources-pass18.js?v=20260810-1';
+  next.defer = true;
+  document.body.appendChild(next);
 })();
