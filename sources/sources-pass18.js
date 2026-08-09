ec975dc9a3aaefd1099fb6e6301beb3f68aa8037
Haohao Zhang <zhhos98@gmail.com>
2026-08-10 05:05:07 +0800

    Chain new source corpus passes after pass18

--- sources/sources-pass18.js
+++ sources/sources-pass18.js
@@ -50,4 +50,9 @@
   });
 
   receivingSection.insertAdjacentElement('afterend', section);
+
+  const next = document.createElement('script');
+  next.src = 'sources-pass19.js?v=20260810-1';
+  next.defer = true;
+  document.body.appendChild(next);
 })();
