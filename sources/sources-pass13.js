29797d773df165ce805ba070c22d1fec15a9ed00
Haohao Zhang <zhhos98@gmail.com>
2026-08-10 03:52:57 +0800

    Chain payment regulation source pass after packing feedback

--- sources/sources-pass13.js
+++ sources/sources-pass13.js
@@ -50,4 +50,9 @@
   });
 
   forwarderSection.insertAdjacentElement('afterend', section);
+
+  const next = document.createElement('script');
+  next.src = 'sources-pass14.js?v=20260810-1';
+  next.defer = true;
+  document.body.appendChild(next);
 })();
